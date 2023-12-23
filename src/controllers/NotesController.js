dc604af069d6ec5411d25d307a7c10aae5120a06
Caua <cauahenrique5@outlook.com>
2023-12-23 11:38:20 -0300

    comentarios NotesConroller.js

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,16 +1,19 @@
 const knex = require('../database/knex')
 
 class NotesController {
+  // Método assíncrono para criar uma nova nota com títulos, descrição, tags e links.
   async create(request, response) {
     const { title, description, tags, links } = request.body
     const { user_id } = request.params
 
+    // Inserção dos dados principais da nota na tabela 'notes'.
     const [note_id] = await knex('notes').insert({
       title,
       description,
       user_id
     })
 
+    // Mapeamento dos links para serem inseridos na tabela 'links'.
     const linksInsert = links.map(link => {
       return {
         note_id,
@@ -18,8 +21,10 @@ class NotesController {
       }
     })
 
+    // Inserção dos links na tabela 'links'.
     await knex('links').insert(linksInsert)
 
+    // Mapeamento das tags para serem inseridas na tabela 'tags'.
     const tagsInsert = tags.map(name => {
       return {
         note_id,
@@ -28,18 +33,25 @@ class NotesController {
       }
     })
 
+    // Inserção das tags na tabela 'tags'.
     await knex('tags').insert(tagsInsert)
 
+    // Resposta JSON indicando sucesso na criação da nota.
     response.json()
   }
 
+  // Método assíncrono para buscar e exibir uma nota específica com suas tags e links.
   async show(request, response) {
     const { id } = request.params
 
+    // Busca da nota na tabela 'notes'.
     const note = await knex('notes').where({ id }).first()
+    // Busca das tags relacionadas à nota na tabela 'tags'.
     const tags = await knex('tags').where({ note_id: id }).orderBy('name')
+    // Busca dos links relacionados à nota na tabela 'links'.
     const links = await knex('links').where({ note_id: id }).orderBy('created_at')
 
+    // Resposta JSON contendo os detalhes da nota, suas tags e links.
     return response.json({
       ...note,
       tags,
@@ -47,22 +59,29 @@ class NotesController {
     })
   }
 
+  // Método assíncrono para deletar uma nota específica.
   async delete(request, response) {
     const { id } = request.params
 
+    // Deleção da nota na tabela 'notes'.
     await knex('notes').where({ id }).delete()
 
+    // Resposta JSON indicando sucesso na deleção da nota.
     return response.json()
   }
 
+  // Método assíncrono para buscar e retornar notas com base em filtros.
   async index(request, response) {
     const { title, user_id, tags } = request.query
 
     let notes
 
+    // Verificação se há filtros de tags.
     if (tags) {
+      // Divisão e formatação das tags recebidas.
       const filterTags = tags.split(',').map(tag => tag.trim())
 
+      // Busca das notas com base nos filtros de tags.
       notes = await knex('tags')
         .select([
           'notes.id',
@@ -76,14 +95,18 @@ class NotesController {
         .orderBy('notes.title')
         
     } else {
+      // Busca das notas sem filtros de tags.
       notes = await knex('notes')
-      .where({ user_id })
-      .whereLike('title', `%${title}%`)
-      .orderBy('title')
+        .where({ user_id })
+        .whereLike('title', `%${title}%`)
+        .orderBy('title')
     }
 
+    // Busca das tags do usuário.
     const userTags = await knex('tags').where({ user_id })
-    const notesWhithTags = notes.map(note => {
+    
+    // Mapeamento das notas com suas tags.
+    const notesWithTags = notes.map(note => {
       const noteTags = userTags.filter(tag => tag.note_id === note.id)
 
       return {
@@ -92,7 +115,8 @@ class NotesController {
       }
     })
 
-    return response.json(notesWhithTags)
+    // Resposta JSON contendo as notas com suas tags.
+    return response.json(notesWithTags)
   }
 }
 
